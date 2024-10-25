c363f01667a807dcbb94f25e199e59961959fd25
Abiral koju <abiral.koju@gmail.com>
2024-10-25 23:53:49 +0545

    react query

--- e-commerce/src/pages/Home.jsx
+++ e-commerce/src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import { useQuery } from "@tanstack/react-query";
 
 const Home = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: () =>
+      fetch("https://fakestoreapi.com/products").then((res) => res.json()),
+  });
+
+  if (error) return <div>Your API call was error!</div>;
+
+  if (isLoading) return <h4>Your api call is loading</h4>;
   return (
     <div>
       <Navbar />
+      {data?.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <h3>{product.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
